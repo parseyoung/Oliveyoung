56883e3d07d6a26e79b31da02e7d390c29f27988
GiSeong Ji <jiggyjiggy0323@gmail.com>
2024-08-27 17:40:08 +0900

    Abstract View manage

--- client/ClientManager.h
+++ client/ClientManager.h
@@ -2,20 +2,21 @@
 #define CLIENT_MANAGER_H
 
 #include "../include/BaseManager.h"
+#include "../include/ViewManager.h"
 #include "Client.h"
 #include "../database/Constants.h"
 
 using namespace std;
 
-class ClientManager : public BaseManager<Client>
+class ClientManager : public BaseManager<Client>, public ViewManager
 {
 public:
     ClientManager();
 
     // View 관리
-    const bool displayMenu();
-    void displayItemsInfo() const;
-    void inputItem();
+    const bool displayMenu() override;
+    void displayItemsInfo() const override;
+    void inputItem() override;
 
 private:
 };
